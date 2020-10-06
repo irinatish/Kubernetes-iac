0008892e7d3ff4b92d94b6848ed5da0e0d418266
irinatish <25349696+irinatish@users.noreply.github.com>
2020-10-06 12:22:34 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,6 +20,31 @@ resource "aws_lambda_function" "test_lambda" {
     }
   }
 }
+resource "aws_s3_bucket" "foo-bucket" {
+  region        = var.region
+  bucket        = local.bucket_name
+  force_destroy = true
+
+  tags = {
+    Name = "foo-${data.aws_caller_identity.current.account_id}"
+  }
+  versioning {
+    enabled = true
+  }
+  logging {
+    target_bucket = "${aws_s3_bucket.log_bucket.id}"
+    target_prefix = "log/"
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.mykey.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+  acl           = "private"
+}
 resource "aws_instance" "compute_host" {
   # ec2 have plain text secrets in user data
   ami           = "ami-04169656fea786776"
