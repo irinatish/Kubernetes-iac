5fa8e0dcec20269afba972421670193dc4b1fa49
irinatish <25349696+irinatish@users.noreply.github.com>
2020-10-06 13:12:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,3 @@
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-}
-resource "aws_lambda_function" "test_lambda" {
-  filename      = "resources/lambda_function_payload.zip"
-  function_name = "${local.resource_prefix.value}-analysis"
-  role          = "${aws_iam_role.iam_for_lambda.arn}"
-  handler       = "exports.test"
-
-  source_code_hash = "${filebase64sha256("resources/lambda_function_payload.zip")}"
-
-  runtime = "nodejs12.x"
-
-  environment {
-    variables = {
-      access_key = "AKIAIOSFODNN7EXAMPLE"
-      secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-    }
-  }
-}
 resource "aws_s3_bucket" "foo-bucket" {
   region        = var.region
   bucket        = local.bucket_name
@@ -43,25 +21,5 @@ resource "aws_s3_bucket" "foo-bucket" {
       }
     }
   }
-  acl           = "private"
-}
-resource "aws_instance" "compute_host" {
-  # ec2 have plain text secrets in user data
-  ami           = "ami-04169656fea786776"
-  instance_type = "t2.nano"
-  user_data     = <<EOF
-#! /bin/bash
-sudo apt-get update
-sudo apt-get install -y apache2
-sudo systemctl start apache2
-sudo systemctl enable apache2
-export AWS_ACCESS_KEY_ID
-export AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE
-export AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
-export AWS_DEFAULT_REGION=us-west-2
-echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
-EOF
-  tags = {
-    Name  = "${local.resource_prefix.value}-ec2"
-  }
+  acl           = "public-read"
 }
