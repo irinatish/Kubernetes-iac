7928f8906b76754e0a9b8192178620816bb7c396
irinatish <25349696+irinatish@users.noreply.github.com>
2020-10-06 11:08:00 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAIOSFODNN7EXAMPLE"
+  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+}
+
 #------------------------------------------------------------------------------
 # ECS Cluster
 #------------------------------------------------------------------------------
